d2c3edd0a6118f019f46470f6d2f8f7c4fa3dacd
LucianoR23 <231097lr@gmail.com>
2023-03-09 15:54:51 -0300

    125 useFet

--- src/02-useEffect/FormWithCustomHook.jsx
+++ src/02-useEffect/FormWithCustomHook.jsx
@@ -1,24 +1,18 @@
 import { useEffect, useState } from "react"
-import { Message } from "./Message"
+import { useForm } from "../hooks/useForm"
+// import { Message } from "./Message"
 
 
-export const SimpleForm = () => {
+export const FormWithCustomHook = () => {
 
-    const [formState, setFormState] = useState({
+
+    const {formState, onInputChange, username, email, password, onResetForm} = useForm({
         username: '',
-        email: ''
+        email: '',
+        password: ''
     })
 
-    const {username, email} = formState
-
-    const onInputChange = ({target}) => {
-        const {name, value} = target;
-        setFormState({
-            ...formState,
-            [ name ]: value
-        })
-    }
-
+    // const {username, email, password} = formState;
 
     // useEffect(() => {
     //     console.log('useEffect llamado email');
@@ -29,7 +23,7 @@ export const SimpleForm = () => {
 
   return (
     <>
-        <h1>Formulario Simple</h1>
+        <h1>Formulario con Custom Hook</h1>
         <hr />
 
         <input 
@@ -40,10 +34,10 @@ export const SimpleForm = () => {
             value={username}
             onChange={onInputChange} />
         
-        {
+        {/* {
             (username === 'LucianoR23') && <Message/>
-        }
-        
+        } */}
+
         <input 
             type="text"
             className="form-control mt-2"
@@ -51,6 +45,16 @@ export const SimpleForm = () => {
             name="email"
             value={email}
             onChange={onInputChange} />
+        
+        <input 
+            type="password"
+            className="form-control mt-2"
+            placeholder="Contraseña"
+            name="password"
+            value={password}
+            onChange={onInputChange} />
+
+        <button onClick={onResetForm} className="btn btn-primary mt-2">Borrar</button>
 
     </>
   )
